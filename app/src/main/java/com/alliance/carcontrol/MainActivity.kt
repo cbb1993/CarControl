02046372b8f1995533f35a906a6a2da5d4f6b58d
cbb1993 <1253202418@qq.com>
2019-04-08 15:14:01 +0800

    some edit

--- app/src/main/java/com/alliance/carcontrol/MainActivity.kt
+++ app/src/main/java/com/alliance/carcontrol/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.alliance.carcontrol
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import com.base.baselib.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+class MainActivity : BaseActivity() {
+    override fun getLayoutId(): Int {
+       return R.layout.activity_main
     }
+
+
 }
