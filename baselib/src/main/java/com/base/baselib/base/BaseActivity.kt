4953ed25ba7300247601b71ebe15add3a92bad7f
yang <d1066936839@163.com>
2019-04-08 15:42:40 +0800

    fitutil

--- baselib/src/main/java/com/base/baselib/base/BaseActivity.kt
+++ baselib/src/main/java/com/base/baselib/base/BaseActivity.kt
@@ -19,6 +19,7 @@ import com.base.baselib.utils.PreferenceUtil
 import com.base.data.dao.DaoSession
 import com.base.event.NetworkChangeEvent
 import com.base.baselib.receiver.NetworkChangeReceiver
+import com.base.baselib.utils.FitUtil
 import com.readystatesoftware.systembartint.SystemBarTintManager
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -27,22 +28,22 @@ import org.greenrobot.eventbus.ThreadMode
 
 abstract class BaseActivity : AppCompatActivity() {
 
-     /**
-      * 缓存上一次的网络状态
-      */
-     protected var hasNetwork: Boolean by PreferenceUtil(Constant.HAS_NETWORK_KEY, true)
+    /**
+     * 缓存上一次的网络状态
+     */
+    protected var hasNetwork: Boolean by PreferenceUtil(Constant.HAS_NETWORK_KEY, true)
 
-     /**
-      * 网络状态变化的广播
-      */
-     var mNetworkChangeReceiver: NetworkChangeReceiver? = null
+    /**
+     * 网络状态变化的广播
+     */
+    var mNetworkChangeReceiver: NetworkChangeReceiver? = null
 
-     /**
-      * 提示View 有无网络
-      */
-     lateinit var mTipView: View
-     lateinit var mWindowManager: WindowManager
-     lateinit var mLayoutParams: WindowManager.LayoutParams
+    /**
+     * 提示View 有无网络
+     */
+    lateinit var mTipView: View
+    lateinit var mWindowManager: WindowManager
+    lateinit var mLayoutParams: WindowManager.LayoutParams
 
 
     /**
@@ -52,13 +53,14 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //适配屏幕
+        FitUtil.autoFit(this, false, application);
         //状态栏
         initStatusBar(getStatusBarColor())
         // 获取布局id
         setContentView(getLayoutId())
 
-        if(useEventBus()){
+        if (useEventBus()) {
             EventBus.getDefault().register(this)
         }
 
@@ -68,14 +70,14 @@ abstract class BaseActivity : AppCompatActivity() {
         initViews()
     }
 
-     override fun onResume() {
-         // 动态注册网络变化广播
-         val filter = IntentFilter()
-         filter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
-         mNetworkChangeReceiver = NetworkChangeReceiver()
-         registerReceiver(mNetworkChangeReceiver, filter)
-         super.onResume()
-     }
+    override fun onResume() {
+        // 动态注册网络变化广播
+        val filter = IntentFilter()
+        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
+        mNetworkChangeReceiver = NetworkChangeReceiver()
+        registerReceiver(mNetworkChangeReceiver, filter)
+        super.onResume()
+    }
 
     /**
      * 修改状态栏颜色，支持4.4以上版本
@@ -96,24 +98,24 @@ abstract class BaseActivity : AppCompatActivity() {
         }
     }
 
-     /**
-      * 初始化 TipView
-      */
-     private fun initTipView() {
-         mTipView = layoutInflater.inflate(R.layout.layout_network_tip, null)
-         mWindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-         mLayoutParams = WindowManager.LayoutParams(
-             ViewGroup.LayoutParams.MATCH_PARENT,
-             ViewGroup.LayoutParams.WRAP_CONTENT,
-             WindowManager.LayoutParams.TYPE_APPLICATION,
-             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-             PixelFormat.TRANSLUCENT)
-         mLayoutParams.gravity = Gravity.TOP
-         mLayoutParams.x = 0
-         mLayoutParams.y = 0
-         mLayoutParams.windowAnimations = R.style.anim_float_view // add animations
-     }
-
+    /**
+     * 初始化 TipView
+     */
+    private fun initTipView() {
+        mTipView = layoutInflater.inflate(R.layout.layout_network_tip, null)
+        mWindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        mLayoutParams = WindowManager.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_APPLICATION,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            PixelFormat.TRANSLUCENT
+        )
+        mLayoutParams.gravity = Gravity.TOP
+        mLayoutParams.x = 0
+        mLayoutParams.y = 0
+        mLayoutParams.windowAnimations = R.style.anim_float_view // add animations
+    }
 
 
     /**
@@ -126,23 +128,23 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     private fun checkNetwork(isConnected: Boolean) {
-            if (isConnected) {
-                if (mTipView != null && mTipView.parent != null) {
-                    mWindowManager.removeView(mTipView)
-                }
-            } else {
-                if (mTipView.parent == null) {
-                    mWindowManager.addView(mTipView, mLayoutParams)
-                }
+        if (isConnected) {
+            if (mTipView != null && mTipView.parent != null) {
+                mWindowManager.removeView(mTipView)
             }
+        } else {
+            if (mTipView.parent == null) {
+                mWindowManager.addView(mTipView, mLayoutParams)
+            }
+        }
     }
 
 
-     /**
-      * 初始化views
-      *
-      */
-     open fun initViews(){}
+    /**
+     * 初始化views
+     *
+     */
+    open fun initViews() {}
 
 
     abstract fun getLayoutId(): Int
@@ -152,8 +154,7 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
 
-
-   private fun setTranslucentStatus() {
+    private fun setTranslucentStatus() {
         val winParams = window.attributes;
         winParams.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
         window.attributes = winParams
@@ -169,20 +170,19 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        if(useEventBus()){
+        if (useEventBus()) {
             EventBus.getDefault().unregister(this)
         }
     }
 
 
-    open fun navigation(path:String,v:View){
-        var compat = ActivityOptionsCompat.
-        makeScaleUpAnimation(v, v.width / 2, v.height / 2, 0, 0);
+    open fun navigation(path: String, v: View) {
+        var compat = ActivityOptionsCompat.makeScaleUpAnimation(v, v.width / 2, v.height / 2, 0, 0);
 
         ARouter.getInstance().build(path).withOptionsCompat(compat).navigation()
     }
 
-    fun getDaoSession() :DaoSession{
-       return BaseApplication.getDaoSession()
+    fun getDaoSession(): DaoSession {
+        return BaseApplication.getDaoSession()
     }
 }
---
+++ baselib/src/main/java/com/base/baselib/utils/FitUtil.java
@@ -0,0 +1,51 @@
+package com.base.baselib.utils;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
+
+public class FitUtil {
+    private static float width = 480;//手动设置为设计图的宽,适配将根据宽为基准,也可以设置高,但是推荐设 置宽,如果不需要px=dp则不设置也行
+    private static int dpi = 320;//手动设置设计图的dpi,不知道可以设计图的宽除2测试一下
+    private static float nativeWidth = 0;//真实屏幕的宽,不需要手动改
+    private static float sRoncompatDennsity;
+    private static float sRoncompatScaledDensity;
+
+    /**
+     * 在Activity的onCreate中调用,修改该Activity的density,即可完成适配,使用宽高直接使用设计图上px相等的dp值
+     *
+     * @param activity     需要改变的Activity
+     * @param isPxEqualsDp 是否需要设置为设计图上的px直接在xml上写dp值(意思就是不需要自己计算dp值,直接写设计图上的px值,并改单位为dp),但开启后可能需要手动去设置ToolBar的大小,如果不用可以忽略
+     */
+    public static void autoFit(Activity activity, boolean isPxEqualsDp, final @NonNull Application application) {
+        if (nativeWidth == 0) {
+            nativeWidth = activity.getWindowManager().getDefaultDisplay().getWidth();
+        }
+        DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
+        if (sRoncompatDennsity == 0) {
+            sRoncompatDennsity = displayMetrics.density;
+            sRoncompatScaledDensity = displayMetrics.scaledDensity;
+            //如果想要字体随系统设置字体大小就加上监听，否则不需要
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    if (newConfig != null && newConfig.fontScale > 0) {
+                        sRoncompatScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+
+                }
+            });
+        }
+        displayMetrics.density = isPxEqualsDp ? nativeWidth / dpi / (width / dpi) : nativeWidth / dpi;
+        displayMetrics.densityDpi = (int) (displayMetrics.density * 160);
+        //适配字体sp
+        displayMetrics.scaledDensity = (displayMetrics.density) * (sRoncompatScaledDensity / sRoncompatDennsity);
+    }
+}
