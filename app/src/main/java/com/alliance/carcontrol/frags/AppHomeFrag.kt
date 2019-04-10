5bdbd509bd8a560bf5268f9ed3db4789aa3fda60
cbb1993 <1253202418@qq.com>
2019-04-10 18:09:26 +0800

    some edit

--- app/src/main/java/com/alliance/carcontrol/frags/AppHomeFrag.kt
+++ app/src/main/java/com/alliance/carcontrol/frags/AppHomeFrag.kt
@@ -3,6 +3,7 @@ package com.alliance.carcontrol.frags
 import android.view.View
 import com.alliance.carcontrol.R
 import com.base.baselib.base.BaseFragment
+import kotlinx.android.synthetic.main.app_frag_home.*
 
 /**
  * Created by 斌斌.
@@ -18,6 +19,51 @@ class AppHomeFrag : BaseFragment(){
 
     override fun initView(view: View) {
 
+
+        btn_back.setOnClickListener {
+            if(rl_content.isShown){
+                rl_content.visibility = View.GONE
+            }
+        }
+
+        btn_break.setOnClickListener {
+           setIv1Show(R.mipmap.home_break)
+        }
+        btn_air.setOnClickListener {
+            setIv1Show(R.mipmap.home_air)
+        }
+        btn_travel.setOnClickListener {
+            setIv1Show(R.mipmap.home_travel)
+        }
+        btn_charging.setOnClickListener {
+            setIv1Show(R.mipmap.home_charging)
+        }
+        btn_power.setOnClickListener {
+            setIv1Show(R.mipmap.home_power)
+        }
+
+
+
+        btn_left.setOnClickListener {
+            iv_1.visibility=View.VISIBLE
+            iv_2.visibility=View.GONE
+        }
+
+        btn_right.setOnClickListener {
+            iv_1.visibility=View.GONE
+            iv_2.visibility=View.VISIBLE
+        }
+
+
+    }
+
+
+    fun setIv1Show(res :Int){
+        rl_content.visibility = View.VISIBLE
+        iv_1.visibility=View.VISIBLE
+        iv_2.visibility=View.GONE
+
+        iv_1.setImageResource(res)
     }
 
     override fun lazyLoad() {
