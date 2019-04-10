ab148af592b19c72d05c7822fe787c5a98b931cf
cbb1993 <1253202418@qq.com>
2019-04-10 15:40:45 +0800

    someEdit

--- app/src/main/java/com/alliance/carcontrol/MainActivity.kt
+++ app/src/main/java/com/alliance/carcontrol/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alliance.carcontrol
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import com.alliance.carcontrol.frags.*
 import com.base.baselib.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,17 +25,12 @@ class MainActivity : BaseActivity() {
     override fun initViews() {
         super.initViews()
         initFragment()
-
-
-        iv_.setOnClickListener {
-            iv_.isActivated = !iv_.isActivated
-        }
     }
 
 
     private fun initFragment() {
         rg_menu.setOnCheckedChangeListener { _, checkedId ->
-            when(checkedId){
+            when (checkedId) {
                 R.id.rb_phone -> setIndexSelected(0)
                 R.id.rb_music -> setIndexSelected(1)
                 R.id.rb_home -> setIndexSelected(2)
@@ -49,18 +45,29 @@ class MainActivity : BaseActivity() {
         val appGuideFrag = AppGuideFrag()
         val appSettingFrag = AppSettingFrag()
 
-        mFragments = listOf(appPhoneFrag, appMusicFrag, appHomeFrag,appGuideFrag, appSettingFrag)
+        mFragments = listOf(appPhoneFrag, appMusicFrag, appHomeFrag, appGuideFrag, appSettingFrag)
 
         val ft = supportFragmentManager.beginTransaction()
         ft.add(R.id.main_content, mFragments[mIndex]).commit()
 
         rg_menu.check(R.id.rb_home)
+        iv_nav.setImageResource(R.mipmap.menu_line_home)
 
     }
+
     private fun setIndexSelected(index: Int) {
         if (mIndex == index) {
             return
         }
+        when(index){
+            0 -> iv_nav.setImageResource(R.mipmap.menu_line_phone)
+            1 -> iv_nav.setImageResource(R.mipmap.menu_line_music)
+            2 -> iv_nav.setImageResource(R.mipmap.menu_line_home)
+            3 -> iv_nav.setImageResource(R.mipmap.menu_line_nav)
+            4 -> iv_nav.setImageResource(R.mipmap.menu_line_setting)
+        }
+
+
         val ft = supportFragmentManager.beginTransaction()
         ft.hide(mFragments[mIndex])
 //        //判断是否添加
@@ -75,8 +82,6 @@ class MainActivity : BaseActivity() {
         mIndex = index
 
     }
-
-
 
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
