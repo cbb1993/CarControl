bf536bd47f734d34eb17c51d6912319065cce8ad
yang <d1066936839@163.com>
2019-04-11 16:53:30 +0800

    jump app

--- app/src/main/java/com/alliance/carcontrol/frags/AppHomeFrag.kt
+++ app/src/main/java/com/alliance/carcontrol/frags/AppHomeFrag.kt
@@ -1,5 +1,7 @@
 package com.alliance.carcontrol.frags
 
+import android.content.Intent
+import android.text.TextUtils
 import android.view.View
 import com.alliance.carcontrol.R
 import com.base.baselib.base.BaseFragment
@@ -12,12 +14,20 @@ import kotlinx.android.synthetic.main.app_frag_home.*
  * describe: 主页展示车况
  */
 
-class AppHomeFrag : BaseFragment(){
+class AppHomeFrag : BaseFragment() {
 
     override fun attachLayoutRes(): Int {
         return R.layout.app_frag_home
     }
+
     override fun initView(view: View) {
+        btn_home_app.setOnClickListener {
+            val intentForPackage = this@AppHomeFrag.context?.packageManager?.getLaunchIntentForPackage("com.neusoft.weixindemo")
+            if (intentForPackage != null) {
+                intentForPackage.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intentForPackage)
+            }
+        }
     }
 
     override fun lazyLoad() {
