6dbebab2e748122537a2b7e21b12537cc7693558
yang <d1066936839@163.com>
2019-04-11 13:58:22 +0800

    nav

--- app/src/main/java/com/alliance/carcontrol/frags/AppMusicFrag.kt
+++ app/src/main/java/com/alliance/carcontrol/frags/AppMusicFrag.kt
@@ -1,8 +1,11 @@
 package com.alliance.carcontrol.frags
 
+import android.support.annotation.DrawableRes
+import android.support.annotation.RawRes
 import android.view.View
 import com.alliance.carcontrol.R
 import com.base.baselib.base.BaseFragment
+import kotlinx.android.synthetic.main.app_frag_music.*
 
 /**
  * Created by 斌斌.
@@ -17,7 +20,26 @@ class AppMusicFrag : BaseFragment(){
     }
 
     override fun initView(view: View) {
+        btn_mu_back.setOnClickListener {
+            setIvMiMenubg(R.mipmap.music_menu)
+        }
+        btn_local_radio.setOnClickListener {
+            setIvMibg(R.mipmap.music_local_radio)
+        }
+        btn_web_radio.setOnClickListener {
+            setIvMibg(R.mipmap.music_web_radio)
+        }
+    }
 
+    fun setIvMiMenubg(@DrawableRes int: Int){
+        btn_local_radio.visibility = View.VISIBLE
+        btn_web_radio.visibility = View.VISIBLE
+        iv_mu_bg.setImageResource(int)
+    }
+    fun setIvMibg(@DrawableRes int: Int){
+        btn_local_radio.visibility = View.GONE
+        btn_web_radio.visibility = View.GONE
+        iv_mu_bg.setImageResource(int)
     }
 
     override fun lazyLoad() {
