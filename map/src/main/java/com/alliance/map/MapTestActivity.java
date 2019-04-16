ddcd6ee7c2608d7ca4891257db68d7e17da7cc63
yang <d1066936839@163.com>
2019-04-16 10:50:56 +0800

    multiDex

--- map/src/main/java/com/alliance/map/MapTestActivity.java
+++ map/src/main/java/com/alliance/map/MapTestActivity.java
@@ -1,10 +1,15 @@
 package com.alliance.map;
 
 import android.util.Log;
+import android.view.View;
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.amap.api.navi.AmapNaviPage;
+import com.amap.api.navi.AmapNaviParams;
+import com.amap.api.navi.INaviInfoCallback;
+import com.amap.api.navi.model.AMapNaviLocation;
 import com.base.baselib.base.BaseActivity;
 
 import java.text.SimpleDateFormat;
@@ -65,7 +70,89 @@ public class MapTestActivity extends BaseActivity implements AMapLocationListene
         // 在定位结束后，在合适的生命周期调用onDestroy()方法
         // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
         //启动定位
-        mlocationClient.startLocation();
+//        mlocationClient.startLocation();
+        findViewById(R.id.btn_naviPage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AmapNaviPage.getInstance().showRouteActivity(MapTestActivity.this,
+                        new AmapNaviParams(null), new INaviInfoCallback() {
+                            @Override
+                            public void onInitNaviFailure() {
+
+                            }
+
+                            @Override
+                            public void onGetNavigationText(String s) {
+
+                            }
+
+                            @Override
+                            public void onLocationChange(AMapNaviLocation aMapNaviLocation) {
+
+                            }
+
+                            @Override
+                            public void onArriveDestination(boolean b) {
+
+                            }
+
+                            @Override
+                            public void onStartNavi(int i) {
+
+                            }
+
+                            @Override
+                            public void onCalculateRouteSuccess(int[] ints) {
+
+                            }
+
+                            @Override
+                            public void onCalculateRouteFailure(int i) {
+
+                            }
+
+                            @Override
+                            public void onStopSpeaking() {
+
+                            }
+
+                            @Override
+                            public void onReCalculateRoute(int i) {
+
+                            }
+
+                            @Override
+                            public void onExitPage(int i) {
+
+                            }
+
+                            @Override
+                            public void onStrategyChanged(int i) {
+
+                            }
+
+                            @Override
+                            public View getCustomNaviBottomView() {
+                                return null;
+                            }
+
+                            @Override
+                            public View getCustomNaviView() {
+                                return null;
+                            }
+
+                            @Override
+                            public void onArrivedWayPoint(int i) {
+
+                            }
+
+                            @Override
+                            public void onMapTypeChanged(int i) {
+
+                            }
+                        });
+            }
+        });
     }
 
     @Override
