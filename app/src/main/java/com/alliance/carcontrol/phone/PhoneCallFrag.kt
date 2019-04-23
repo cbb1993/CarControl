f12a1668f6d5877e92c59b0c20e25513d4ba50fe
cbb1993 <1253202418@qq.com>
2019-04-23 13:06:08 +0800

    edit

--- app/src/main/java/com/alliance/carcontrol/phone/PhoneCallFrag.kt
+++ app/src/main/java/com/alliance/carcontrol/phone/PhoneCallFrag.kt
@@ -1,8 +1,17 @@
 package com.alliance.carcontrol.phone
 
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.alliance.carcontrol.R
+import com.base.baselib.adapter.CommonAdapter
+import com.base.baselib.adapter.ViewHolder
 import com.base.baselib.base.BaseFragment
+import kotlinx.android.synthetic.main.frag_phone_call.*
+
 
 /**
  * Created by 斌斌.
@@ -11,7 +20,7 @@ import com.base.baselib.base.BaseFragment
  * describe: 拨号页面
  */
 
-class PhoneCallFrag : BaseFragment(){
+class PhoneCallFrag : BaseFragment() {
     override fun attachLayoutRes(): Int {
         return R.layout.frag_phone_call
     }
@@ -21,9 +30,55 @@ class PhoneCallFrag : BaseFragment(){
     }
 
 
-
+    private val list = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#")
+    private val buffer: StringBuffer = StringBuffer()
     override fun lazyLoad() {
+        disableShowSoftInput(ev_number)
+        ev_number.requestFocus()
+        ev_number.isLongClickable = false
 
+        recycler_number.layoutManager = GridLayoutManager(activity, 3)
+        recycler_number.adapter = object : CommonAdapter<String>(activity, list, R.layout.item_number) {
+            override fun convert(holder: ViewHolder, t: MutableList<String>) {
+                val info = t[holder.realPosition]
+                val tv_number = holder.getView<TextView>(R.id.tv_number)
+                tv_number.text = info
+                holder.getView<View>(R.id.root).setOnClickListener {
+                    buffer.append(info)
+                    ev_number.setText(buffer.toString())
+                    ev_number.setSelection(ev_number.length())
+                }
+            }
+        }
+        iv_delete.setOnClickListener {
+            if (buffer.isNotEmpty()) {
+                var index = ev_number.selectionStart
+                if (index > 0) {
+                    index -= 1
+                    buffer.deleteCharAt(index)
+                    ev_number.setText(buffer.toString())
+                    ev_number.setSelection(index)
+                }
+            }
+        }
+
+
+        recycler_people.layoutManager = LinearLayoutManager(activity)
+        recycler_people.adapter = object :CommonAdapter<String>(activity, list,R.layout.item_people){
+            override fun convert(holder: ViewHolder?, t: MutableList<String>?) {
+            }
+        }
+
+    }
+
+    private fun disableShowSoftInput(editText: EditText) {
+        try {
+            val cls: Class<EditText> = EditText::class.java
+            val method = cls.getMethod("setShowSoftInputOnFocus", Boolean::class.java)
+            method.isAccessible = true
+            method.invoke(editText, false)
+        } catch (e: Exception) {
+        }
     }
 
 
